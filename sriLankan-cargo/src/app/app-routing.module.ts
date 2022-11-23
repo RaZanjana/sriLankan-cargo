534984a10d1040d60f1cc803c7859c7f787e835d
23707 <23707@srilankan.com>
2022-11-23 14:29:52 +0530

    create componets

--- sriLankan-cargo/src/app/app-routing.module.ts
+++ sriLankan-cargo/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MasterPageComponent } from './master-page/master-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component:MasterPageComponent,
+    path:'',
+    children:[
+      {
+        component:HomePageComponent,
+        path:''
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sriLankan-cargo/src/app/app.module.ts
+++ sriLankan-cargo/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MasterPageComponent } from './master-page/master-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MasterPageComponent,
+    HomePageComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
